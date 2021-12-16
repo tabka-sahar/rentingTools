b19a70c8f1f41f1fa01cc389dd051eb066577e1c
BelguesmiSakr <belguesmi_sakr@yahoo.com>
2021-12-16 02:02:29 +0100

    working on update profile

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -8,6 +8,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { NavbareComponent } from './navbare/navbare.component';
+import { UpdateProfileComponent } from './update-profile/update-profile.component';
 
 @NgModule({
   declarations: [
@@ -15,8 +16,19 @@ import { NavbareComponent } from './navbare/navbare.component';
     AppComponent,
     UserProfileComponent,
     NavbareComponent,
+    UpdateProfileComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent },
+      { path: 'user', component: UserProfileComponent },
+      { path: 'user/update', component: UpdateProfileComponent },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
